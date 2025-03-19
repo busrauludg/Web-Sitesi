27989fe9be9709ccdf4108436282a1414f92fd71
busrauludg <busrapc.226@gmail.com>
2025-03-19 14:55:50 +0300

    Kullanıcı girişi
    veritabanına bağlı

--- Görev-Yöneticisi/GorevYonetim/Controllers/UserController.cs
+++ Görev-Yöneticisi/GorevYonetim/Controllers/UserController.cs
@@ -18,7 +18,32 @@ namespace GorevYonetim.Controllers
             var model = _context.Users.ToList();
             return View(model);
         }
-        
+       
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SignUp([FromForm]User model)
+        {
+            if(model.Password.Length<7||!model.Password.Any(char.IsLetter)||!model.Password.Any(char.IsDigit)||!model.Password.Any(c=>!char.IsLetterOrDigit(c)))
+            {
+                ModelState.AddModelError("Password", "Password must be at least 7 characters long, contain 4 letters, 2 digits, and 1 special character.");
+    
+            }
+            if(_context.Users.Any(u=>u.Email==model.Email))
+            {
+                 ModelState.AddModelError("Email", "This email is already registered.");
+            }
+
+            if(ModelState.IsValid)
+            {
+                _context.Users.Add(model);//kullanıcıyı veritabanına ekler
+                _context.SaveChanges();//değişiklikleri kaydedin
+                return RedirectToAction("Index");//başarıyla eklerinsi indexe döndücek
+            }
+            return View();
+        }
 
     }
 }
